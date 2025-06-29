ef37b16f612d3663e91e26f4e4dd447461be8a7d
claudiabdm <44007726+claudiabdm@users.noreply.github.com>
2025-06-29 17:22:16 +1200

    Add slash

--- functions/api/dictionary/all.ts
+++ functions/api/dictionary/all.ts
@@ -2,7 +2,7 @@ import { WordEntries } from "../../../src/types";
 import { formatEntry, replacer } from "../../_utils";
 
 export async function onRequestGet({ env }) {
-    const object = await env.DICTIONARY.get('dictionary');
+    const object = await env.DICTIONARY.get('/dictionary');
 
     if (object === null) {
         return new Response("Object Not Found", { status: 404 });
